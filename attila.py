90cf1c9edc75c9ec031223d8495de30740f6527d
Maxime Boucher <mxboucher@gmail.com>
2025-01-10 15:17:16 -0800

    refactor: working on constraints modelling at scale

--- attila.py
+++ attila.py
@@ -4,7 +4,7 @@
 # PuLP is an linear and mixed integer programming modeler written in Python.
 from pulp import *
 
-from twoptimizer import Building, Province, Region
+from twoptimizer import Building, Province, Region, RegionAttila, RegionHasPort, RegionHasRessource, RegionType, Games
 
 """
 A province contains n regions.
@@ -16,7 +16,6 @@ A region has sanitation, public order, economy, religion, and food.
 
 We need to optimize using linear programming the economy of a province, here using PuLP.
 """
-from collections import defaultdict
 
 """
 ('cha_effect_imperium', 'faction_to_faction_own_unseen', '10.0000'), ('att_effect_economy_gdp_trade_local', 'building_to_building_own', '200.0000'), ('att_effect_public_order_base', 'province_to_province_own', '2.0000'), ('att_effect_region_resource_wine_production', 'building_to_building_own', '10.0000'), ('att_effect_economy_gdp_trade_local', 'building_to_building_own', '300.0000'), ('att_effect_public_order_base', 'province_to_province_own', '4.0000'), ('att_effect_region_resource_wine_production', 'building_to_building_own', '15.0000'), ('att_effect_economy_gdp_trade_local', 'building_to_building_own', '400.0000'), ('att_effect_public_order_base', 'province_to_province_own', '6.0000'), ('att_effect_region_resource_wine_production', 'building_to_building_own', '20.0000'), ('att_effect_economy_gdp_trade_local', 'building_to_building_own', '200.0000'), ('att_effect_province_growth_building', 'province_to_province_own', '2.0000'), ('att_effect_economy_gdp_trade_local', 'building_to_building_own', '300.0000'), ('att_effect_province_growth_building', 'province_to_province_own', '4.0000'), ('att_effect_economy_gdp_trade_local', 'building_to_building_own', '400.0000'), ('att_effect_province_growth_building', 'province_to_province_own', '6.0000'), ('att_effect_economy_gdp_mod_industry', 'province_to_region_own', '10.0000'), ('att_effect_region_resource_wood_production', 'building_to_building_own', '10.0000'), ('att_effect_economy_gdp_mod_industry', 'province_to_region_own', '20.0000'), ('att_effect_region_resource_wood_production', 'building_to_building_own', '15.0000'), ('att_effect_economy_gdp_mod_industry', 'province_to_region_own', '30.0000'), ('att_effect_region_resource_wood_production', 'building_to_building_own', '20.0000')]})
@@ -30,28 +29,39 @@ if __name__ == '__main__':
         data = data.split('\n')
         data = [i.split('\t') for i in data]
 
+    # Create a dictionary of buildings per game / DLC
     # dict{game1: set{building1, building2, ...}, game2: set{building1, building2, ...}, ...}
-    games = {}
     for name, effect, scope, amount, _, _ in data:
         game = name.split("_")[0]
-        if game not in games:
-            games[game] = {}
+        if game not in Games.buildings:
+            Games.buildings[game] = {}
         # Filter for att and maximize only gdp for now
         if "att" in name:
             # Filter only for east romans for now
-            if ("roman" in name and "west" not in name) or ("orthodox" in name) or ("all" in name):
-                if name not in games[game]:
-                    games[game][name] = Building(name)
-                games[game][name].add_effect(effect, scope, float(amount))
+            if name not in Games.buildings[game]:
+                Games.buildings[game][name] = Building(name)
+            Games.buildings[game][name].add_effect(effect, scope, float(amount))
 
-    # PuLP model
+    # Create a province, with regions, and buildings constraints.
+    thrace = Province(3, "Thrace")
+    constantinople = Region(6, "Constantinople")
+    marcianopolis = Region(4, "Marcianopolis")
+    trimontium = Region(4, "Trimontium")
+
+    thrace.add_region(constantinople)
+    thrace.add_region(marcianopolis)
+    thrace.add_region(trimontium)
+
+    constantinople.add_buildings(RegionAttila(RegionType.ATTILA_REGION_MAJOR, RegionHasPort.ATTILA_REGION_PORT, RegionHasRessource.ATTILA_REGION_CHURCH))
+    marcianopolis.add_buildings(RegionAttila(RegionType.ATTILA_REGION_MINOR, RegionHasPort.ATTILA_REGION_NO_PORT, RegionHasRessource.ATTILA_REGION_NO_RESSOURCE))
+    trimontium.add_buildings(RegionAttila(RegionType.ATTILA_REGION_MINOR, RegionHasPort.ATTILA_REGION_NO_PORT, RegionHasRessource.ATTILA_REGION_GOLD))
+
+    # Linear programming problem
     # Create a gdp maximization problem
-    problem = LpProblem("GDP Maximization", LpMaximize)
-    # Constraint: Maximum number of buildings to select
-    max_buildings = 6
+    problem = Games.problem
 
     # Create decision variables and include mandatory building condition
-    for building in games["att"].values():
+    for building in Games.buildings["att"].values():
         if "att_bld_roman_east_city_major_4" in building.name:
             # Mandatory building
             building.add_lp_variable(1, 1, LpInteger)
@@ -59,28 +69,32 @@ if __name__ == '__main__':
             # Optional building
             building.add_lp_variable(0, 1, LpInteger)
 
-    thrace = Province(3)
-    constantinople = Region(6)
-    marcianopolis = Region(4)
-    trimontium = Region(4)
-
-    thrace.add_region(constantinople)
-    thrace.add_region(marcianopolis)
-    thrace.add_region(trimontium)
-
-
     # Objective function: Maximize GDP
     problem += lpSum(
         building.gdp() * building.lp_variable
-        for building in games["att"].values()
+        for building in Games.buildings["att"].values()
     ), "Total_GDP"
 
-    # Constraint: Maximum number of buildings (excluding mandatory one)
-    problem += (
-        lpSum(building.lp_variable for building in games["att"].values() if "att_bld_roman_east_city_major_4" not in building.name)
-        <= max_buildings - 1,
-        "Max_Optional_Buildings"
-    )
+    # Step 5: Constraints
+    # Global public order constraint
+    problem += lpSum(
+        building.public_order() * building.lp_variable
+        for region in thrace.regions
+        for building in region.buildings
+    ) >= 1, "Public_Order_Constraint"
+
+    # Regional sanitation constraints
+    for region in thrace.regions:
+        problem += lpSum(
+            building.sanitation() * building.lp_variable for building in region.buildings
+        ) >= 1, f"Sanitation_Constraint_{region.name}"
+
+    # Regional building count constraints
+    for region in thrace.regions:
+        problem += lpSum(
+            building.lp_variable for building in region.buildings
+        ) <= region.n_buildings, f"Max_Buildings_{region.name}"
+
 
     # Solve the problem
     problem.solve()
@@ -88,5 +102,5 @@ if __name__ == '__main__':
     # Print the variables equal to 1 with their respective contribution
     for v in problem.variables():
         if v.varValue == 1:
-            print(v.name, "=", games["att"][v.name].gdp())
+            print(v.name, "=", Games.buildings["att"][v.name].gdp())
 
--- twoptimizer.py
+++ twoptimizer.py
@@ -1,15 +1,38 @@
 import abc
+import enum
 from collections import defaultdict
+from dataclasses import dataclass
 from typing import List
 
-from pulp import LpVariable, LpInteger
+from pulp import LpVariable, LpInteger, LpProblem, LpMaximize, lpSum
+
+
+class AttilaFactions(enum.Enum):
+    """
+    Enum containing a list of factions in Attila.
+    """
+    ATTILA_NONE = 0
+    ATTILA_ROMAN_EAST = 1
+    ATTILA_ROMAN_WEST = 2
+    ATTILA_FRANKS = 3
+    ATTILA_VANDALS = 4
+    ATTILA_VISIGOTHS = 5
+    ATTILA_ALAMANS = 6
+    ATTILA_SAXONS = 7
+    ATTILA_GEPIDS = 8
+    ATTILA_LANGOBARDS = 9
+    ATTILA_BURGUNDIANS = 10
+    ATTILA_OSTROGOTHS = 11
+    ATTILA_SUEBI = 12
+    ATTILA_HUNS = 13
+    ATTILA_ALANS = 14
 
 
 class Games:
-    def __init__(self):
-        self.games = {}
-        self.current = None
-
+    problem = LpProblem("GDP Maximization", LpMaximize)
+    buildings = {}
+    current_game = "att"
+    faction = AttilaFactions.ATTILA_NONE
 
 class Effect(abc.ABC):
     @abc.abstractmethod
@@ -29,11 +52,11 @@ class Effect(abc.ABC):
         pass
 
 
-"""
-A building contains effects that can be applied to a province, region, or building.
-We need to create a Building class that contains a list of effects.
-"""
 class Building(Effect):
+    """
+    A building contains effects that can be applied to a province, region, or building.
+    We need to create a Building class that contains a list of effects.
+    """
     def __init__(self, name: str):
         self.lp_variable = None
         self.name = name
@@ -122,16 +145,161 @@ class Building(Effect):
         return food_production - food_consumption
 
 
-"""
-A region contains buildings.
-"""
-class Region(Effect):
-    def __init__(self, n_buildings: int):
-        self.buildings: List[Building] = []
-        self.effects = defaultdict(list)
+class RegionType(enum.Enum):
+    ATTILA_REGION_MAJOR = 1
+    ATTILA_REGION_MINOR = 2
 
-    def add_building(self, building: Building):
-        self.buildings.append(building)
+class RegionHasPort(enum.Enum):
+    ATTILA_REGION_NO_PORT = 1
+    ATTILA_REGION_PORT = 2
+
+class RegionHasRessource(enum.Enum):
+    ATTILA_REGION_NO_RESSOURCE = 1
+    ATTILA_REGION_FURS = 2
+    ATTILA_REGION_IRON = 3
+    ATTILA_REGION_WINE = 4
+    ATTILA_REGION_WOOD = 5
+    ATTILA_REGION_GOLD = 6
+    ATTILA_REGION_MARBLE = 7
+    ATTILA_REGION_GEMS = 8
+    ATTILA_REGION_SILK = 9
+    ATTILA_REGION_SPICE = 10
+    ATTILA_REGION_SALT = 11
+    ATTILA_REGION_LEAD = 12
+    ATTILA_REGION_OLIVES = 13
+    ATTILA_REGION_CHURCH = 14
+
+@dataclass
+class RegionAttila:
+    region_type: RegionType
+    has_port: RegionHasPort
+    has_ressource: RegionHasRessource
+
+
+def building_is_major(building_name: str) -> bool:
+    """
+    Check if a building is major (city or town).
+    :param building_name:
+    :return:
+    """
+    if "major" in building_name:
+        return True
+    return False
+
+def building_is_minor(building_name: str) -> bool:
+    """
+    Check if a building is minor (village).
+    :param building_name:
+    :return:
+    """
+    if "minor" in building_name or "agriculture" in building_name:
+        return True
+    return False
+
+def building_is_not_of_faction(building_name: str) -> bool:
+    """
+    Check if a building is of the faction currently assigned in Games.
+    :param building_name:
+    :return:
+    """
+    if Games.faction == AttilaFactions.ATTILA_ROMAN_EAST and ("roman" in building_name and "west" not in building_name) or ("orthodox" in building_name) or ("all" in building_name):
+        return False
+    return True
+
+
+class Region(Effect):
+    """
+    A region contains buildings.
+    """
+    def __init__(self, n_buildings: int, name: str):
+        self.n_buildings = n_buildings  # Number of buildings that can be built in the region. NOT equal to len(buildings).
+        self.buildings: List[Building] = []  # List of buildings that are potentially fit for the region.
+        self.effects = defaultdict(list)
+        self.name = name
+
+    def add_buildings(self, region: RegionAttila):
+        """
+        Add buildings to the region.
+        It should filter buildings based on the region type, port, and resource.
+        Adapt constraints accordingly.
+        For example, number of free buildings decrease if the region has a port.
+        Building must be named region_building.
+        :param region:
+        :return:
+        """
+        # Add buildings Lp variables to the region.
+        for building in Games.buildings[Games.current_game].values():
+            deep_copy = Building(f"{self.name}_{building.name}")
+            if building_is_not_of_faction(building.name):
+                continue
+            if region.region_type == RegionType.ATTILA_REGION_MAJOR and building_is_minor(building.name):
+                continue
+            if region.region_type == RegionType.ATTILA_REGION_MINOR and building_is_major(building.name):
+                continue
+            self.buildings.append(deep_copy)
+        # Add constraints to the region.
+        self.add_type_constraint(region)
+        self.add_resource_constraint(region)
+        self.add_port_constraint(region)
+
+    def add_port_constraint(self, region: RegionAttila):
+        """
+        If the region has a port, then we can add a constraint that the number of buildings in the region with "port" is between 1 and 1.
+        :param region:
+        :return:
+        """
+        if region.has_port == RegionHasPort.ATTILA_REGION_PORT:
+            Games.problem += lpSum(
+                building.lp_variable
+                for building in self.buildings
+                if "port" in building.name and "spice" not in building.name
+            ) == 1, "Port_Constraint"
+        else:
+            Games.problem += lpSum(
+                building.lp_variable
+                for building in self.buildings
+                if "port" in building.name and "spice" not in building.name
+            ) == 0, "No_Port_Constraint"
+
+    def add_resource_constraint(self, region: RegionAttila):
+        """
+        If the region has a resource, then we can add a constraint that the number of buildings in the region with "resource" and "spice" is between 1 and 1. That's because
+        spice resource is mandatory (is a port). Any other resource is optional.
+        :param region:
+        :return:
+        """
+        if region.has_ressource != RegionHasRessource.ATTILA_REGION_NO_RESSOURCE:
+            Games.problem += lpSum(
+                building.lp_variable
+                for building in self.buildings
+                if "resource" in building.name and "spice" in building.name
+            ) == 1, "Spice_Resource_Constraint"
+        else:
+            Games.problem += lpSum(
+                building.lp_variable
+                for building in self.buildings
+                if "resource" in building.name and "port" not in building.name or "spice" in building.name
+            ) == 0, "No_Resource_Constraint"
+
+    def add_type_constraint(self, region):
+        """
+        If the region is major, then we can add a constraint that all buildings with "minor" are between 0 and 0, as well as "agriculture".
+        Conversely, disable civic, major buildings in minor regions.
+        :param region:
+        :return:
+        """
+        if region.region_type == RegionType.ATTILA_REGION_MAJOR:
+            Games.problem += lpSum(
+                building.lp_variable
+                for building in self.buildings
+                if "minor" in building.name or "agriculture" in building.name
+            ) == 0, "No_Minor_Constraint"
+        else:
+            Games.problem += lpSum(
+                building.lp_variable
+                for building in self.buildings
+                if "major" in building.name or "civic" in building.name
+            ) == 0, "No_Major_Constraint"
 
     def gdp(self):
         """
@@ -161,13 +329,15 @@ class Region(Effect):
         """
         return sum([building.public_order() for building in self.buildings])
 
-"""
-We need to create a Province class that contains a list of regions.
-"""
+
 class Province(Effect):
-    def __init__(self, n_regions: int):
+    """
+    We need to create a Province class that contains a list of regions.
+    """
+    def __init__(self, n_regions: int, name: str):
         self.regions = []
         self.n_regions = n_regions
+        self.name = name
 
     def add_region(self, region: Region):
         self.regions.append(region)
